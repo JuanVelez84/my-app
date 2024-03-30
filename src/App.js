ada1afd65547dad34e13e1fe1cf098479a9f6d9f
JuanVelez84 <juanjosevr2002@gmail.com>
2024-03-30 11:55:32 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ListaUsuarios from './ListaUsuario';
+import AgregarUsuarios from './AgregarUsuario';
+import EditarUsuarioss from './EditarUsuario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD MERN</h1>
+      <ListaUsuarios/>
+      <AgregarUsuarios/>
+      <EditarUsuarioss/>
     </div>
   );
 }
